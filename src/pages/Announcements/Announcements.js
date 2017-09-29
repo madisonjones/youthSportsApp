afcbfe6ba6824622e60b96efbcf6d2530ffcb621
Josh <joshuaeup@gmail.com>
2017-09-28 23:58:33 -0400

    Layout for signIn or signUp dashboard done

--- src/pages/Announcements/Announcements.js
+++ src/pages/Announcements/Announcements.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const Announcements = () => {
-    
-}
+const Announcements = props => {
+  <div className="container">
+    <h1>Discussion</h1>
+    <hr/>
+    <div className="card">
+      <div className="card-block">
+        <h4 className="card-title">{props.title}</h4>
+        <p className="card-text">
+          {props.message}
+        </p>
+      </div>
+      <div className="card-footer">
+        <small className="text-muted">{props.time}</small>
+      </div>
+    </div>
+  </div>;
+};
 
-export default Announcements
+export default Announcements;
