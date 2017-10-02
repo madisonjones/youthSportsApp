162317114ffe8c9bdbdbd44493eac7b37fd2b08c
Josh <joshuaeup@gmail.com>
2017-10-01 23:11:41 -0400

    added css to Announcements page

--- src/pages/Announcements/Announcements.js
+++ src/pages/Announcements/Announcements.js
@@ -11,19 +11,19 @@ const Announcements = props => {
     <div className="row h-100">
       <div className="col-2 collapse d-md-flex bg-faded pt-2 h-100" id="sidebar">
         <ul className="nav flex-column">
-          <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-          <li className="nav-item"><a className="nav-link" href="#">Announcments</a></li>
-          <li className="nav-item"><a className="nav-link" href="#">Discussion</a></li>
-          <li className="nav-item"><a className="nav-link" href="#">Highlights</a></li>
+          <li className="nav-item"><a className="nav-link customNavBtn" href="#">Home</a></li>
+          <li className="nav-item"><a className="nav-link customNavBtn" href="#">Announcements</a></li>
+          <li className="nav-item"><a className="nav-link customNavBtn" href="#">Discussion</a></li>
+          <li className="nav-item"><a className="nav-link customNavBtn" href="#">Highlights</a></li>
         </ul>
       </div>
       
-      <div className="col pt-2">
-        <h1 className="customColor text-center customCategory">Announcments</h1>
-        <a href="" data-target="#sidebar" data-toggle="collapse" className="hidden-md-up"><i className="fa fa-bars"></i></a>
-        <div className="container d-flex align-items-center" id="wrapper">
+      <div className="col pt-2 mainBackground">
+        <h1 className="customColor text-center customCategory">Announcements</h1>
+        <a href="" data-target="#sidebar" data-toggle="collapse" class="hidden-md-up"><i class="fa fa-bars"></i></a>
+        <div className="container d-flex align-items-center " id="wrapper">
           <div id="customContainer">
-            <div className="card card-inverse">
+            <div className="card card-inverse customBackground">
               <div className="card-block ">
                 
                 <div className="card customCard">
@@ -57,7 +57,7 @@ const Announcements = props => {
                 //   <div className="card-block">
                 //     <h4 className="card-title text-info">{props.title}</h4>
                 //     <h6 className="card-subtitle mb-2 text-muted">{props.time}</h6>
-                //     <p className="card-text text-info">{props.message}</p>
+                //     <p claclassNamess="card-text text-info">{props.message}</p>
                 //   </div>
                 // </div>
 
@@ -77,7 +77,7 @@ const Announcements = props => {
                 //     <h6 className="card-subtitle mb-2 text-muted">{props.time}</h6>
                 //     <p className="card-text text-info">{props.message}</p>
                 //   </div>
-                // </div>
+                // </div> 
 
 
               </div>
